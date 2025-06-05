4bef45051a475db7157f07018b2265be075fb71b
DNCApp997 <b.dinc2018@gmail.com>
2025-06-05 13:35:14 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DNCBauApp from "./DNCBauApp";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DNCBauApp />
+  </React.StrictMode>
+);
